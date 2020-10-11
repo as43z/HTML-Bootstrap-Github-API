69374b7e21ac69636d2ae3d0ffb7a89eba221fc8
Albert S <albertsigmaeng@gmail.com>
2020-10-11 18:08:10 +0200

    Test Buttons and alerts

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-$(document).ready(function(){
-    $("#btn-hid").hide();
-    $("#btn").click(function(){
-        $("#btn-hid").show();
-    });
-});
-
 function Clicked(){
     $("#alert-info").toggle();
 }
