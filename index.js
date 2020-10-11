2926e11b93a039641eaa74f19605d68a3612f843
Albert S <albertsigmaeng@gmail.com>
2020-10-11 18:59:16 +0200

    Start working on the exercise

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function Clicked(){
-    $("#alert-info").toggle();
-}
+$(document).ready(function(){
+    $("input:text").val("Input a valid name...");
+    $("#user-inp").click(function(){
+        $("input:text").val("");
+    });
+    $("#btn").click(function(){
+        $("input:text").val("ASD");
+    });
+});
+
+//function Clicked(){
+//    $("#alert-info").toggle();
+//}
+
+//function onSelect(){
+//    $("input:text").val("");
+//}
